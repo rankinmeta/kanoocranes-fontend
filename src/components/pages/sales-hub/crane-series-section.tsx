4d2766f1cc36455ab88281c422e40e4fd79ce285
Prajil K <prajilk20017@gmail.com>
2026-09-15 11:40:24 +0530

    img placement fix

--- src/components/pages/sales-hub/crane-series-section.tsx
+++ src/components/pages/sales-hub/crane-series-section.tsx
@@ -1,6 +1,6 @@
 import HighlightedTitle from "@/components/common/highlight-title";
 import { StrapiImage } from "@/components/common/strapi-image";
-import { extractHighlightText } from "@/lib/utils";
+import { cn, extractHighlightText } from "@/lib/utils";
 import type { CraneSeriesSectionProps } from "@/type";
 import ModelTable from "../rental-hub/table";
 
@@ -9,7 +9,7 @@ const CraneSeriesSection = ({
   table,
 }: CraneSeriesSectionProps) => {
   return (
-    <section className="bg-[#f5f5f5 bg-[#f0efef]">
+    <section className="bg-[#f0efef]">
       <div className="relative z-10 container container-padding-x py-10 md:py-16 lg:py-20 grid md:grid-cols-5 gap-5 items-center">
         <StrapiImage
           src={crane_series.image.url}
@@ -19,7 +19,12 @@ const CraneSeriesSection = ({
           className="hidden md:block aspect-[1/0.7] col-span-2 rounded-md object-cover"
         />
 
-        <div className="md:col-span-3">
+        <div
+          className={cn(
+            "md:col-span-3",
+            crane_series.img_placement === "right" && "order-first",
+          )}
+        >
           <div className="space-y-2.5">
             <HighlightedTitle
               title={crane_series.title}
